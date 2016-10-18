940a5cce5951c564248459a75b242cd0179e9db0
Primiano Tucci <primiano@bitleaks.net>
2016-10-19 00:51:32 +0100

    Fix ACME challenge/response security

--- handlers/letsencrypt.py
+++ handlers/letsencrypt.py
@@ -1,6 +1,6 @@
 import webapp2
 
-class MainPage(webapp2.RequestHandler):
+class ChallengeHandler(webapp2.RequestHandler):
     def get(self, challenge):
         app = webapp2.get_app()
         response = app.registry.get(challenge)
@@ -10,12 +10,17 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(response)
 
-    def post(self, challenge):
+class ResponseHandler(webapp2.RequestHandler):
+    def get(self, challenge, response):
         self.response.headers['Content-Type'] = 'text/plain'
         app = webapp2.get_app()
-        app.registry[challenge] = self.request.body
+        app.registry[challenge] = response
         self.response.write('OK')
 
+    def post(self, challenge, response):
+      return self.get(challenge, response)
+
 app = webapp2.WSGIApplication([
-    ('/.well-known/acme-challenge/(.*)', MainPage),
+    ('/.well-known/acme-challenge/(.*)', ChallengeHandler),
+    ('/.well-known/acme-response/([^/]+)/(.*)', ResponseHandler),
 ], debug=True)
